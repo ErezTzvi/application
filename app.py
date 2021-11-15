50f52c489c98e8e55acd864469416774f78f333b
ErezTzvi <e.yirmiya@gmail.com>
2021-11-15 14:33:37 +0200

    update

--- app.py
+++ app.py
@@ -9,11 +9,40 @@ student_list = sorted([student for student in os.listdir(students_path) if stude
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route("/") # default route. returns a list of students in an html format. Clicking on a students opens the info.
+@app.route("/") # default route. returns a list of ALL students in an html format. Clicking on a students opens the info.
 def main_page():
-    return(html_list_of_students(student_list))
+    return(html_list_of_students(student_list))+"""
+    <form method="POST" action="/search">
+    <input name="pattern">
+    <input type="submit" value="Search">
+    </form>
+    """ # return list of all students plus option to search specific pattern in the json files
 
+@app.route("/student/<json_file>")
+def show_json(json_file):
+    f = open(students_path / json_file)
+    data = json.load(f)
+    f.close()
+    html = """
+    <h1>Information: {}</h1>
+    <p>
+    {}
+    </p>
+    """.format(json_file.rstrip(".json"),"<br>".join(["{}: {}".format(key,value) for key,value in data.items() if value!=None]))
+    return(html)
 
+@app.route("/search", methods=['POST'] )
+def list_students_with_pattern():
+    pattern = request.form.get('pattern')
+    filtered_list = []
+    for student in student_list:
+        f = open(students_path / student)
+        txt = f.read()
+        f.close()
+        if pattern in txt:
+            filtered_list.append(student)
+    return(html_list_of_students(filtered_list)).replace("Students in the Course:","Students with pattern '{}'".format(pattern))
+        
 
 
 def html_list_of_students(json_files):
@@ -34,3 +63,5 @@ def html_list_of_students(json_files):
     </ul>
     """.format("".join(["<li><a href=/student/"+student+">"+student.rstrip(".json")+"</a></li>" for student in json_files]))
     return(html)
+
+app.run(port="8043",host="0.0.0.0")
